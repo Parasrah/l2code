ab1fdcd05ed56ea34f7f5aa26be0f839f023b494
Parasrah <git@parasrah.com>
2020-03-21 16:50:41 -0600

    setting up repo

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+fn add_one (input: isize) -> isize {
+    unimplemented!()
+}
+
+fn not (input: bool) -> bool {
+    unimplemented!()
+}
+
+fn list_add_one (input: &Vec<isize>) -> Vec<isize> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_one_succeeds () {
+        // assign
+        let input = 0;
+        let expected = 1;
+
+        // act
+        let output = add_one(input);
+
+        // assert
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn not_succeeds () {
+        // assign
+        let input = true;
+        let expected = false;
+
+        // act
+        let output = not(input);
+
+        // assert
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn list_add_one_succeeds () {
+        // assign
+        let input = vec![1, 2, 3];
+        let expected = vec![2, 3, 4];
+
+        // act
+        let output = list_add_one(&input);
+
+        // assert
+        for (&a, &b) in output.iter().zip(expected.iter()) {
+            assert_eq!(a, b);
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+fn main() {
+    /*---------------------*/
+    /*     hello world     */
+    /*---------------------*/
+
+    /*------------------*/
+    /*     comments     */
+    /*------------------*/
+
+    /*-------------------*/
+    /*     variables     */
+    /*-------------------*/
+
+    /*-------------------*/
+    /*     operators     */
+    /*-------------------*/
+
+    // =, *, +
+
+    /*---------------------------------------------*/
+    /*     basic types shared by all languages     */
+    /*---------------------------------------------*/
+
+    /*--------------------*/
+    /*     mutability     */
+    /*--------------------*/
+
+    // What is mutability?
+
+    // Rust is one of the few languages where
+    // it's easy to reason about mutability.
+    // That is why it's hard to learn.
+
+    /*---------------------*/
+    /*     concurrency     */
+    /*---------------------*/
+
+    /*-----------------------*/
+    /*     borrow system     */
+    /*-----------------------*/
+
+    // rust's borrow system
+    // 
+    // I want to avoid this as much as possible.
+    // This is why Rust is a complicated language.
+    // It's also Rust's best feature.
+    // I don't think it will be useful for your
+    // learning to focus too much on it right now.
+    //
+    // From the Rust book:
+    // ===================
+    //
+    // * Each value in Rust has a variable that’s called its owner.
+    // * There can only be one owner at a time.
+    // * When the owner goes out of scope, the value will be dropped.
+    //
+    // The "borrow" operator is "&"
+
+    /*----------------------*/
+    /*     control flow     */
+    /*----------------------*/
+
+    // all programming languages have some form of control flow.
+    // different programming languages will offer you different
+    // kinds of control flow.
+
+    // pattern matching
+
+    // if else
+
+    /*-------------------*/
+    /*     functions     */
+    /*-------------------*/
+
+    /*------------------*/
+    /*     closures     */
+    /*------------------*/
+
+    /*---------------*/
+    /*     lists     */
+    /*---------------*/
+
+    /*-------------------*/
+    /*     iterators     */
+    /*-------------------*/
+
+    /*------------------------*/
+    /*     error handling     */
+    /*------------------------*/
+}
