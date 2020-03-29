311fa2814006a6327d277be929c0a85843bd3509
Parasrah <git@parasrah.com>
2020-03-28 23:32:57 -0600

    checkpoint (march 28, 2020)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::fmt;
 
 fn main() {
-    data_types_example();
+    custom_traits_example();
 }
 
 // camelCase
@@ -15,18 +15,8 @@ fn main() {
 
 // data types
 
-fn data_types_example () {
-    numbers_example();
-    boolean_example();
-    character_example();
-    strings_example();
-    array_example();
-    list_example();
-    function_example();
-}
-
 fn numbers_example () {
-    // numbers (i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64)
+    // numbers (i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, fsize)
     #![allow(overflowing_literals)]
     let my_num: i8 = -129;
     println!("my_num: {}", my_num);
@@ -35,6 +25,7 @@ fn numbers_example () {
 fn boolean_example () {
     let my_bool = true;
     println!("the opposite of {} is {}", my_bool, !my_bool);
+    println!("5 is greater than 3 is {}", 5 > 3);
 }
 
 fn character_example () {
@@ -49,13 +40,13 @@ fn strings_example () {
 
 fn tuple_example () {
     // tuples have a fixed length, but can contain different types
-    let my_tuple = (5, true);
+    let my_tuple = (5, String::from("hello world"));
     println!("look! I made a tuple! {:#?}", my_tuple); // quick note - why the {:#?}?
 
     // for two tuples to have the same "type", they must contain the
     // same length, with the same types in the same order
     let my_similar_tuple = (10, false);
-    let my_different_tuple = (10.5, String::from("hi"));
+    let my_different_tuple = (11, true);
 }
 
 fn array_example () {
@@ -82,12 +73,18 @@ fn list_example () {
     // variable length. a list must be made up of the same
     // type.
     my_list.push(4);
+    println!("{:#?}", my_list);
 }
 
 fn function_example () {
     let input = 5;
     let my_fun = |x| x + 1;
     print!("adding 1 to {} gives me {}", input, my_fun(input));
+}
+
+fn get_fn () -> Box<dyn Fn() -> i32> {
+    let input = 5;
+    return Box::new(move || input + 1);
 }
 
 /*--------------------*/
@@ -100,6 +97,10 @@ fn function_example () {
 // it's easy to reason about mutability.
 // That is why it's hard to learn.
 // what is memory?
+
+fn mut_example () {
+
+}
 
 /*-----------------------*/
 /*     borrow system     */
@@ -119,39 +120,43 @@ fn function_example () {
 // * Each value in Rust has a variable that’s called its owner.
 // * There can only be one owner at a time.
 // * When the owner goes out of scope, the value will be dropped.
-//
+
 // The "borrow" operator is "&"
 
 fn ownership_example () {
     let my_str = String::from("hello");
-    let updated = borrow(&my_str);
-    println!("before: {}", my_str);
+    let updated = own(my_str);
+    // println!("before: {}", my_str);
     println!("after: {}", updated);
 }
 
 fn borrow (my_str: &str) -> String {
     let mut updated = String::from(my_str);
-    updated.push_str("world!");
+    updated.push_str(" world!");
     updated
 }
 
 fn own (mut my_str: String) -> String {
-    my_str.push_str("world!");
+    my_str.push_str(" world!");
     my_str
 }
 
 // control flow
 
 fn control_flow_example () {
-    let my_var = 1;
-    if my_var == 1 {
-        println!("my variable was 1!");
-    }
+    let my_var = 3;
+    // if my_var == 1 {
+    //     println!("my variable was 1!");
+    // } else if my_var == 2 {
+    //     println!("my variable was 2!");
+    // } else {
+    //     println!("my variable was not 1!");
+    // }
 
     let output = match my_var {
         1 => String::from("was 1"),
         2 => String::from("was 2!"),
-        value => format!("was {}", value),
+        _ => format!("was unkwon"),
     };
 
     println!("my_var {}", output);
@@ -193,10 +198,10 @@ struct Person {
 }
 
 impl Person {
-    fn new (name: String, age: u16, gender: Gender) -> Person {
+    pub fn new (name: String, years: u16, gender: Gender) -> Person {
         Person {
             name: name,
-            age: age,
+            age: years,
             // shortcut syntax
             gender,
         }
@@ -224,7 +229,7 @@ fn complex_types_example () {
         name: String::from("Canada"),
         political_ideology: PoliticalIdeology::Democracy(18),
     };
-    let person = Person::new(String::from("Lexi"), 21, Gender::Female);
+    let person = Person::new(String::from("Lexi"), 12, Gender::Female);
     let can_or_cannot = match person.can_vote(&country) {
         true => "can",
         false => "cannot",
@@ -268,6 +273,12 @@ trait Named {
 impl Named for &Person {
     fn get_name (&self) -> &str {
         &self.name
+    }
+}
+
+impl dyn Named {
+    fn get_name_length <T: Named> (input: T) -> usize {
+        input.get_name().len()
     }
 }
 
